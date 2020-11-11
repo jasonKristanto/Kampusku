149862819e41331bef1e3b036538e6dca0d11113
jasonkristanto <jasonkristanto4@gmail.com>
2020-11-11 22:13:07 +0700

    Retrieve Feature

---
+++ resources/views/mahasiswa/main/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('title', 'Mahasiswa')
+
+@section('content')
+    <div id="show-all-mahasiswa">
+        <div class="title text-center">
+            <h1 class="font-weight-bold">Show All Mahasiswa</h1>
+        </div>
+        <div class="content mt-5">
+            <table class="table m-auto text-center">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>NIM</th>
+                    <th>Nama</th>
+                    <th>Jenis Kelamin</th>
+                    <th>Usia</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($mahasiswas as $mahasiswa)
+                    <tr>
+                        <td>{{ $loop->index + 1 }}</td>
+                        <td>{{ $mahasiswa->nim }}</td>
+                        <td>{{ $mahasiswa->nama }}</td>
+                        <td>{{ $mahasiswa->jenis_kelamin }}</td>
+                        <td>{{ $mahasiswa->usia }}</td>
+                        <td>
+                            <a href="#" type="button" class="btn btn-primary btn-sm">
+                                <i class="material-icons" aria-hidden="true">create</i>
+                            </a>
+                            <a href="#" type="button" class="btn btn-danger btn-sm">
+                                <i class="material-icons" aria-hidden="true">delete</i>
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
